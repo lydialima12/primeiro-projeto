0419977be6c22abba09808646c29aaf55af7c469
Lydia Lima <lydialima51@gmail.com>
2020-04-07 11:06:50 -0300

    Criação do projeto Angular
    Criação do primeiro projeto angular para testes

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+template: `
+<p>Meu primeiro componente</p>
+`
+}) //component: afirma que essa classe abaixo é um componente do angular
+export class MeuPrimeiroComponent{ }
